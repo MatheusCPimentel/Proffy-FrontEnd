4221ad57080e1b6b6dfc9e5778a603b029e6a783
Matheus Pimentel <matheus08.pimentel@gmail.com>
2020-08-05 23:24:33 -0300

    Finish the web application with backend connection

--- src/components/TeacherItem/item.tsx
+++ src/components/TeacherItem/item.tsx
@@ -2,33 +2,57 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string; 
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/59742601?s=460&u=b186b69a73a03fe4644daf046e04b70b54d9b130&v=4" alt="Matheus Pimentel"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Matheus Pimentel</strong>
-          <span>Programação</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Especialista em React, React Native e Node.js
-        <br /><br />
-        5 anos já na área.
+        {teacher.bio}
       </p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 200,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
